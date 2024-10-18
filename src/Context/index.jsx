77bf4969937ee621a1ef2b529b72377864b8d143
gabrielwillianfb <gabriel.beck03@gmail.com>
2024-10-18 04:05:05 -0300

    Adicionado busca de localização do usuário automática

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -7,26 +7,36 @@ const StateContext = createContext();
 export const StateContextProvider = ({ children }) => {
   const [weather, setWeather] = useState({});
   const [values, setValues] = useState([]);
-  const [place, setPlace] = useState("São Francisco do Sul");
+  const [place, setPlace] = useState("New York");
   const [thisLocation, setLocation] = useState("");
+
+  // Função para obter localização do usuário
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          fetchWeather(`${latitude},${longitude}`); // Passando coordenadas
+        },
+        (error) => {
+          console.error("Erro ao obter geolocalização: ", error);
+          fetchWeather(place); // Se o usuário negar, use o valor padrão
+        },
+      );
+    } else {
+      alert("Geolocalização não é suportada pelo seu navegador.");
+      fetchWeather(place); // Caso a geolocalização não esteja disponível
+    }
+  };
 
   // fetch api
   const fetchWeather = async (location) => {
-    const apiKey = import.meta.env.VITE_API_KEY; // substitua pela sua chave de API
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}`;
-
-    const options = {
-      method: "GET",
-      params: {
-        unitGroup: "metric",
-        key: apiKey,
-        contentType: "json",
-        location: place,
-      },
-    };
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${apiKey}&contentType=json`;
 
     try {
-      const response = await axios.request({ url, ...options });
+      const response = await axios.get(url);
       const thisData = response.data;
       setLocation(thisData.address);
       setValues(thisData.days);
@@ -37,13 +47,15 @@ export const StateContextProvider = ({ children }) => {
     }
   };
 
+  // obtém a localização do usuário automaticamente
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
+  // Faz a função de pesquisa funcionar
   useEffect(() => {
     fetchWeather(place);
   }, [place]);
-
-  useEffect(() => {
-    console.log("Valores: ", values);
-  }, [values]);
 
   return (
     <StateContext.Provider
